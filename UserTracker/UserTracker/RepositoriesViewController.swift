0fb7b43ae5111fd3f2cd1cdb990b656367f25d87
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-12-14 15:31:15 +0100

    Initial commit

---
+++ UserTracker/UserTracker/RepositoriesViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  UserTracker
+//
+//  Created by Carlos Cortés Sánchez on 14/12/2017.
+//  Copyright © 2017 Carlos Cortés Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class RepositoriesViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
