699395a0d4d94dc3bbe59dad055ec4807f90c38f
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-12-14 16:18:19 +0100

    Add network model

---
+++ UserTracker/UserTracker/RepositoryNetworkModel.swift
@@ -0,0 +1,41 @@
+//
+//  RepositoryNetworkModel.swift
+//  UserTracker
+//
+//  Created by Carlos Cortés Sánchez on 14/12/2017.
+//  Copyright © 2017 Carlos Cortés Sánchez. All rights reserved.
+//
+
+import ObjectMapper
+import RxAlamofire
+import RxCocoa
+import RxSwift
+
+struct RepositoryNetworkModel {
+    
+    lazy var rx_respositories: Driver<[Repository]> = self.fetchRepositories()
+    private var repositoryName: Observable<String>
+    
+    init(withNameObservable nameObservable: Observable<String>) {
+        self.repositoryName = nameObservable
+    }
+    
+    private func fetchRepositories() -> Driver<[Repository]> {
+        return repositoryName
+            .flatMapLatest { text in
+                return RxAlamofire
+                    .requestJSON(.get, "https://api.github.com/users/\(text)/repos")
+                    .debug().catchError { error in
+                        return Observable.never()
+                }
+            }
+            .map { (response,json) -> [Repository] in
+                if let repos = Mapper<Repository>().mapArray(JSONObject: json) {
+                    return repos
+                } else {
+                    return []
+                }
+            }
+            .asDriver(onErrorJustReturn: [])
+    }
+}
