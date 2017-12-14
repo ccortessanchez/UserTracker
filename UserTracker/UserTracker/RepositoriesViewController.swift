e8c6bb573078866c25b7b70c15ee5b0b84635d9e
Carlos Cortés Sánchez <ccortes@gmv.com>
2017-12-14 16:46:44 +0100

    Connect data to view controller

--- UserTracker/UserTracker/RepositoriesViewController.swift
+++ UserTracker/UserTracker/RepositoriesViewController.swift
@@ -14,10 +14,14 @@ import RxSwift
 
 class RepositoriesViewController: UIViewController {
 
+    @IBOutlet weak var tableViewBottomConstraint: NSLayoutConstraint!
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
-    var rx_searchBar: Observable<String> {
+    let disposeBag = DisposeBag()
+    var repositoryNetworkModel: RepositoryNetworkModel!
+    
+    var rx_searchBarText: Observable<String> {
         return searchBar.rx.text
             .filter { $0 != nil }
             .map { $0! }
@@ -32,9 +36,30 @@ class RepositoriesViewController: UIViewController {
     }
     
     func setupRx() {
+        repositoryNetworkModel = RepositoryNetworkModel.init(withNameObservable: rx_searchBarText)
         
+        repositoryNetworkModel
+            .rx_respositories
+            .drive(tableView.rx.items) { (tv,i,repository) in
+                let cell = tv.dequeueReusableCell(withIdentifier: "repositoryCell", for: IndexPath(row: i, section: 0))
+                cell.textLabel?.text = repository.name
+                
+                return cell
+        }
+            .disposed(by: disposeBag)
+        
+        repositoryNetworkModel
+            .rx_respositories.drive(onNext: { repositories in
+                if repositories.count == 0 {
+                    let alert = UIAlertController(title: ":(", message: "No repositories found for this user", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    if self.navigationController?.visibleViewController?.isMember(of: UIAlertController.self) != true {
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+        })
+            .disposed(by: disposeBag)
     }
-
-
+    
 }
 
